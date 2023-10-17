cfa672db780d779bc8ff335950b5d04f09d1cf88
ornstein070313 <137376161+ornstein070313@users.noreply.github.com>
2023-10-17 14:48:41 +0600

    Update authInterface.c

--- library/authInterface.c
+++ library/authInterface.c
@@ -93,7 +93,7 @@ void readUsernum()
 
 void writeUserdata(FILE *file_ptr, int k)
 {
-    fprintf_s(file_ptr,PRINT_FORMAT,acc_num[k].name,acc_num[k].pass);
+    fprintf(file_ptr,PRINT_FORMAT,acc_num[k].name,acc_num[k].pass);
 };
 
 void readUserdata()
