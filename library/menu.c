bd97a7d4da71b94f2c4cb2df9a00306bd84575b2
KabirBD <mdkabirhosen786@gmail.com>
2023-11-19 22:59:08 +0600

    revised

--- library/menu.c
+++ library/menu.c
@@ -44,7 +44,7 @@ int displayMenu()
         // About Page design (We sill move this code into a function later)
         clr();
         logo();
-        printWrap("\n\n This is an all-in-one student assistant for BSCSE students at NSU. It tracks academic progress, showcases course information, provides personalized course suggestions, and includes tools for creating class schedules and calculating CGPA. It efficiently supports multiple users, facilitating seamless tracking of academic journeys for various users.\n\n", 64);
+        printWrap("\n\n This is an all-in-one student assistant for BSCSE students of NSU. It tracks academic progress, showcases course information, provides personalized course suggestions, and includes tools for creating class schedules and calculating CGPA. It efficiently supports multiple users, facilitating seamless tracking of academic journeys for various users.\n\n", 64);
         tab();
         row(9);
         colorPrint(" Developers ", "y");
